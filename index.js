699f07ddc8948a51a7271d4e01f799fd2d0dbbb0
Picachu Wu <q8139q8139@gmail.com>
2017-10-22 09:25:47 +0800

    Using "Hello Bot". Print message to log.

---
+++ index.js
@@ -0,0 +1,21 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+    channelId: 1540241808,
+    channelSecret: '8f4303ea0d34c907e83b5bbf2fca3c54',
+    channelAccessToken: 'URz0aIlfwkPoCvawLRwdhkVS+YV0we0qfXVpmqsY58/qDwNpqnjP6+oGCutcUmQbn1gRpGXcl8jkIYANj2dH5bFVPDes2FhZH9KEyE1RJxHxEFku2Yo/La/WV6fZMa5cGMvZ6MAQgVdZdxDc9BghLwdB04t89/1O/w1cDnyilFU='
+});
+bot.on('message', function (event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
