d0c7af6aaaee591b63e6b7cd2d4bd391df4bf7d1
Picachu Wu <q8139q8139@gmail.com>
2017-10-29 16:50:07 +0800

    Try to fix crash.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var express = require('express');
 var Airtable = require('airtable');
 var Client = require('pg');
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-client.connect();
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+// });
+// client.connect();
 
 var airtableBase = new Airtable({ apiKey: process.env.AirtableApiKey }).base(process.env.AirtableTableKey);
 
