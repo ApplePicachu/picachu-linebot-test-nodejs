c7dd9eadae1649469f8f8877f7925e13a0df14fe
Picachu Wu <q8139q8139@gmail.com>
2017-11-11 19:56:23 +0800

    Try insert and select.

--- index.js
+++ index.js
@@ -124,9 +124,18 @@ var server = app.listen(process.env.PORT || 8080, function () {
             } else {
                 console.log('CREATE ' + res.rows[0]);
                 client.query('INSERT INTO service_users (line_id) VALUES (U828934c2ea1f46a8243398b2fe3e898c)', (err, res) => {
-                    client.query('SELECT * FROM service_users', (err, res) => {
-                        console.log(res.rows[0]);
-                    });
+                    if (err) {
+                        console.log(err.stack);
+                    } else {
+                        client.query('SELECT * FROM service_users', (err, res) => {
+                            if (err) {
+                                console.log(err.stack);
+                            } else {
+                                console.log('SELECT' + res.rows[0]);
+                            }
+                        });
+                    }
+
                 });
             }
         });
