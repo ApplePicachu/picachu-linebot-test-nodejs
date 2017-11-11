c037e5268af66c3b2207078376f9704f58ceb306
Picachu Wu <q8139q8139@gmail.com>
2017-11-11 20:01:58 +0800

    Try to add timestamp

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ var server = app.listen(process.env.PORT || 8080, function () {
                 console.log(err.stack);
             } else {
                 console.log('CREATE ' + res.rows[0]);
-                var text = 'INSERT INTO service_users(line_id) VALUES($1)';
+                var text = 'INSERT INTO service_users(line_id, state_update_time) VALUES($1, now())';
                 client.query(text, ['U828934c2ea1f46a8243398b2fe3e898c'], (err, res) => {
                     if (err) {
                         console.log(err.stack);
