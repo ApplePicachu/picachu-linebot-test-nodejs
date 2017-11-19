1739428775d60447c83e497cde24271cf91d865f
Picachu Wu <q8139q8139@gmail.com>
2017-11-19 10:03:00 +0800

    Query users and print.

--- index.js
+++ index.js
@@ -30,11 +30,41 @@ bot.on('unfollow', function (event) {
 bot.on('message', function (event) {
     console.log(event); //把收到訊息的 event 印出來看看
     const insertUserText = 'INSERT INTO service_users(line_id, line_name, status_update_time) VALUES($1, $2, now())';
-    event.source.profile().then(function (profile) {
-        client.query(insertUserText, ['U828934c2ea1f46a8243398b2fe3e898c', profile.displayName], (err, res) => {
-            if (err) {
+    if (event.source.type == 'user') {
+        event.source.profile().then(function (profile) {
+            client.query(insertUserText, [profile.userId, profile.displayName], (err, res) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    client.query('SELECT * FROM service_users', (err, res) => {
+                        if (err) {
+                            console.log(err.stack);
+                        } else {
+                            console.log('SELECT %j', res.rows[0]);
+                            bot.push(process.env.LineAdminUserID, { type: 'text', text: res.rows[0] });
+                        }
+                    });
+                }
+            }).catch(function (err) {
                 console.log(err.stack);
-            } else {
+                bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
+            });
+        });
+    }
+   
+    if (event.source.userId == process.env.LineAdminUserID) {
+        switch (event.message.text.toLowerCase()) {
+            case 'drop':
+                dropAndCreateTable(client, (err, res) => {
+                    if (err) {
+                        console.log(err.stack);
+                        bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
+                    } else {
+                        bot.push(process.env.LineAdminUserID, { type: 'text', text: 'Drop success.' });
+                    }
+                });
+                break;
+            case 'user':
                 client.query('SELECT * FROM service_users', (err, res) => {
                     if (err) {
                         console.log(err.stack);
@@ -43,24 +73,7 @@ bot.on('message', function (event) {
                         bot.push(process.env.LineAdminUserID, { type: 'text', text: res.rows[0] });
                     }
                 });
-            }
-        }).catch(function (err) {
-            console.log(err.stack);
-            bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
-        });
-    });
-    if (event.source.userId == process.env.LineAdminUserID) {
-        switch (event.message.text.toLowerCase()) {
-        case 'drop':
-            dropAndCreateTable(client, (err, res) => {
-                if (err) {
-                    console.log(err.stack);
-                    bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
-                } else {
-                    bot.push(process.env.LineAdminUserID, { type: 'text', text: 'Drop success.' });
-                }
-            });
-            break;
+                break;
         }
     }
 
@@ -166,39 +179,4 @@ var server = app.listen(process.env.PORT || 8080, function () {
             }
         }
     });
-    // checkTableExists(client, (err, res) => {
-    //     if (err) {
-    //         console.log(err.stack);
-    //     } else {
-    //         if (!res.rows[0].exists) {
-    //             dropAndCreateTable(client, (err, res) => {
-    //                 if (err) {
-    //                     console.log(err.stack);
-    //                 } else {
-    //                     console.log('CREATE ' + res);
-    //                 }
-    //             });
-    //         }
-    //         console.log('Check table exists \n' + res.rows[0].exists + '\n' + JSON.stringify(res));
-    //     }
-    // });
 });
-
-function dropAndCreateTable(client, callback) {
-    client.query('\
-    DROP TABLE IF EXISTS service_users;\
-    CREATE TABLE "service_users" (\
-        "line_id" char(33) NOT NULL UNIQUE,\
-        "line_name" varchar(20),\
-        "user_group" int NOT NULL DEFAULT \'0\',\
-        "status_init" bigint,\
-        "status_init_extra" TEXT,\
-        "status_current" bigint,\
-        "status_current_extra" TEXT,\
-        "info_json" TEXT,\
-        "status_update_time" timestamp with time zone NOT NULL,\
-        CONSTRAINT service_users_pk PRIMARY KEY ("line_id")\
-    ) WITH (\
-    OIDS=FALSE\
-    );', callback);
-};
