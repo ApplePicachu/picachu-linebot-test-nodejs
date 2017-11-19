f606bb950faf087db8434efc055727558ec95923
Picachu Wu <q8139q8139@gmail.com>
2017-11-19 11:23:40 +0800

    Try to figure out problem.

--- index.js
+++ index.js
@@ -37,30 +37,30 @@ bot.on('message', function (event) {
                 //Print user.
                 bot.push(process.env.LineAdminUserID, { type: 'text', text: 'User exists.' + JSON.stringify(res.rows[0]) });
             } else {
-                //Insert user.
-                event.source.profile().then(function (profile) {
-                    sqlManager.insertUser(profile, (err, res) => {
-                        if (err) {
-                            console.log(err.stack);
-                        } else {
-                            client.query('SELECT * FROM service_users', (err, res) => {
-                                if (err) {
-                                    console.log(err.stack);
-                                } else {
-                                    console.log('SELECT %j', res.rows[0]);
-                                    bot.push(process.env.LineAdminUserID, { type: 'text', text: res.rows[0] });
-                                }
-                            });
-                        }
-                    });
-                    client.query(insertUserText, [profile.userId, profile.displayName], ).catch(function (err) {
-                        console.log(err.stack);
-                        bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
-                    });
-                }).catch(function (err) {
-                    console.log(err.stack);
-                    bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
-                });
+                // //Insert user.
+                // event.source.profile().then(function (profile) {
+                //     sqlManager.insertUser(profile, (err, res) => {
+                //         if (err) {
+                //             console.log(err.stack);
+                //         } else {
+                //             client.query('SELECT * FROM service_users', (err, res) => {
+                //                 if (err) {
+                //                     console.log(err.stack);
+                //                 } else {
+                //                     console.log('SELECT %j', res.rows[0]);
+                //                     bot.push(process.env.LineAdminUserID, { type: 'text', text: res.rows[0] });
+                //                 }
+                //             });
+                //         }
+                //     });
+                //     client.query(insertUserText, [profile.userId, profile.displayName], ).catch(function (err) {
+                //         console.log(err.stack);
+                //         bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
+                //     });
+                // }).catch(function (err) {
+                //     console.log(err.stack);
+                //     bot.push(process.env.LineAdminUserID, { type: 'text', text: err.stack });
+                // });
             }
         });
     }
